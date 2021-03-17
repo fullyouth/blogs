e148c069c9f9454ecc4e0dd1ef19b89536894046
zhanghaoqi <zhanghaoqi@shopex.cn>
2021-03-17 18:02:22 +0800

    feat: react

---
+++ docs/arithmetic/寻找和为定值的多个数.js
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode-cn.com/problems/he-wei-sde-liang-ge-shu-zi-lcof/
+ * 两数之和
+ */
+
+/**
+ * https://leetcode-cn.com/problems/3sum/
+ * 三数之和
+ */
+
+/**
+ * 输入两个整数n和sum，从数列1，2，3.......n 中随意取几个数，使其和等于sum，要求将其中所有的可能组合列出来
+ */
+
+
+const list = [1,2,3,4,5,6,7,8]
+const sum = 5
+const n = 2
+function SumOfkNumber(list, sum, n) {
+  if(n <= 0) return 
+
+}
+
+SumOfkNumber(list, sum, n)
